5a115a4a367d292bcb0dde660c442c3c94e2cf93
htsaii <a0955889206@gmail.com>
2023-12-09 12:54:03 -0500

    add theme selection

--- src/modules/gui.py
+++ src/modules/gui.py
@@ -2,6 +2,7 @@
 
 import time
 import threading
+import tkinter as tk
 import customtkinter as ctk
 from ttkbootstrap import ttk
 from PIL import ImageTk
@@ -20,7 +21,7 @@ class GUI:
         """initialize the GUI class."""
         config.gui = self
         #  "dark-blue", "sweetkind"
-        ctk.set_appearance_mode('dark')
+        # ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('dark-blue')
 
         self.root = ctk.CTk()
@@ -28,17 +29,37 @@ class GUI:
         self.root.wm_iconbitmap()
         icon = ImageTk.PhotoImage(file="assets/icon.png")
         self.root.iconphoto(False, icon)
-        # self.root.geometry(GUI.RESOLUTION['DEFAULT'])
         self.root.geometry(f"{GUI.RESOLUTION['DEFAULT'][0]}x{GUI.RESOLUTION['DEFAULT'][1]}")
         self.root.resizable(False, False)
+
+        # configure grid layout (4x4)
+        self.root.grid_columnconfigure(1, weight=1)
+        self.root.grid_columnconfigure((2, 3), weight=0)
+        self.root.grid_rowconfigure((0, 1, 2), weight=1)
+
 
 
         # # Build the GUI
         # self.navigation = ctk.CTkNotebook(self.root)\
         self.view = View(self.root)
 
-        self.navigation = ttk.Notebook(self.root)
-        self.navigation.pack(expand=True, fill="both")
+
+        # # change appearance mode
+        self.root.sidebar_frame = ctk.CTkFrame(self.root, width=140, corner_radius=0)
+        self.root.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.root.sidebar_frame.grid_rowconfigure(4, weight=1)
+
+         # Create appearance_mode_label and set it to the bottom left
+        self.root.appearance_mode_label = ctk.CTkLabel(self.root.sidebar_frame, text="Appearance Mode", anchor="w")
+        self.root.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+
+        self.root.appearance_mode_optionemenu = ctk.CTkOptionMenu(
+            self.root.sidebar_frame, values=["Light", "Dark", "System"],
+            command=self.change_appearance_mode_event)
+        self.root.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+
+        # self.navigation = ttk.Notebook(self.root)
+        # self.navigation.pack(expand=True, fill="both")
 
         # self.navigation.bind("<<NotebookTabChanged>>", self.on_tab_change)
         self.root.focus()
@@ -59,6 +80,9 @@ class GUI:
         while True:
             self.view.minimap.display_minimap()
             time.sleep(delay)
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        ctk.set_appearance_mode(new_appearance_mode)
 
 
 
